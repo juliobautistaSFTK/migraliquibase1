c6b4cb5a273c9854b472b8aecf564c491006491b
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-09-18 16:22:28 -0600

    Create Main.java

---
+++ db-query/src/main/java/com/example/Main.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+
+        try {
+            transaction = session.beginTransaction();
+            Medidas2 medida = new Medidas2();
+            medida.setIdd(1);
+            medida.setTipo(1);
+            medida.setCantidad((short) 10);
+            medida.setTipoCambio("USD");
+            session.save(medida);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+}
