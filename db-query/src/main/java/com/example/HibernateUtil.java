d3c95c26fd0d3f281b2854b82b0d15a1ba868d96
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-09-18 16:21:00 -0600

    Create HibernateUtil.java para iniciar hibernate

---
+++ db-query/src/main/java/com/example/HibernateUtil.java
@@ -0,0 +1,21 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Crear la SessionFactory a partir de hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
